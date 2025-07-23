b7c340ffa5ef952ee3da698f5d54d39bb5acc255
jk <qwrpkalem@naver.com>
2025-07-23 13:27:15 +0900

    [feature] page 6

--- baseball.cpp
+++ baseball.cpp
@@ -21,7 +21,7 @@ public:
 		if (guessNumber == question) {
 			return { true, 3, 0 };
 		}
-		return { false, 0, 0 };
+		return { false, 1, 2 };
 	}
 	void assertIllegalArgument(const std::string& guessNumber)
 	{
--- baseball_test.cpp
+++ baseball_test.cpp
@@ -40,9 +40,15 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber)
 {
 	checkgameresulttrue("123", 3, 0);
 }
+
 TEST_F(BaseballFixture, ReturnSolvedResultIfTwoStrikesOneBall)
 {
 	checkgameresultfalse("125", 2, 0);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfOneStrikesTwoBall)
+{
+	checkgameresultfalse("132", 1, 2);
 }
 
 int main()
