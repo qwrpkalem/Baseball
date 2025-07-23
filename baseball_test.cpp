674f70aa25294d4c8a93a9ecd3debad7d04fd18b
jk <qwrpkalem@naver.com>
2025-07-23 13:00:19 +0900

    [refactoring] pgae 2

--- baseball_test.cpp
+++ baseball_test.cpp
@@ -2,16 +2,24 @@
 #include "baseball.cpp"
 #include "baseball_test.h"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached)
-{
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")),length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar)
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
 {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")),invalid_argument);
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main()
